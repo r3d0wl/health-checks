32bdf31dd62f53135f090a8253668a23d771e409
r3d0wl <nastcloud@gmail.com>
2026-07-09 17:16:16 +0000

    Added a simple network connectivity check along with a check to see if the CPU is being constrained

--- all_checks.py
+++ all_checks.py
@@ -3,13 +3,15 @@
 import sys
 import os
 import shutil
+import socket
+import psutil
 
 def check_reboot():
     """Returns True if the computer has a pending reboot."""
     return os.path.exists("/run/reboot-required")
 
 def check_disk_full(disk, min_gb, min_percent):
-    """Returns True if there isn't enough disk space, False otherwise"""
+    """Returns True if there isn't enough disk space, False otherwise."""
     du = shutil.disk_usage(disk)
     #Calculate the percentage of free space
     percent_free = 100 * du.free /du.total
@@ -23,10 +25,24 @@ def check_root_full():
     """Returns True if the root partition is full, False otherwise."""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
+def check_cpu_constrained():
+     """Returns Trye if the cpu is having too much usage, False otherwise."""
+     return psutil.cpu_percent(1) > 75
+
+def check_no_network():
+     """Returns True if it fails to resolve Google's URL, False otherwise."""
+     try:
+          socket.gethostbyname("www.google.com")
+          return False
+     except:
+          return True
+
 def main():
     checks=[
         (check_reboot, "Pending Reboot."),
-        (check_root_full, "Root partition full")
+        (check_root_full, "Root partition full."),
+        (check_cpu_constrained, "CPU load too high."),
+        (check_no_network, "No working network.")
     ]
     everything_ok=True
     for check, msg in checks:
